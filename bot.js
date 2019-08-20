0176bd2f8b5691adb4d04a9d15cd5d95c90f1ca0
ziadahmed2020 <54190821+ziadahmed2020@users.noreply.github.com>
2019-08-20 12:47:59 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("612693817430376481")
+setInterval(function() {
+channel.send(`spam spamspamspamspamspam`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
+
+client.login(process.env.BOT_TOKEN2);
